bf97e2ed43eca486d9930be1f600721f52052c54
Jiri Gres <gresjirka@gmail.com>
2024-11-29 13:46:41 +0100

    Input_Validation

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import requests
 import math
 import datetime as dt
 import os
+import re
 # from tkinter import ttk
 
 
@@ -30,20 +31,6 @@ class MyEntry(tk.Entry):
         self.variable.set(new)
 
 
-class About(tk.Toplevel):
-    def __init__(self, parent):
-        super().__init__(parent, class_=parent.name)
-        self.config()
-
-        self.lbl = tk.Label(self, text="Chyba")
-        btn = tk.Button(self, text="OK", command=self.close)
-        self.lbl.pack()
-        btn.pack()
-
-    def close(self):
-        self.destroy()
-
-
 class Application(tk.Tk):
     name = basename(splitext(basename(__file__.capitalize()))[0])
     name = "Smenarna"
@@ -56,7 +43,8 @@ class Application(tk.Tk):
 
         self.title(self.name)
         self.bind("<Escape>", self.quit)
-        
+
+        val_f_tuple = (self.register(self.validate), "%P")
 
         self.varAuto = tk.BooleanVar()
         self.currency = tk.StringVar()
@@ -76,27 +64,34 @@ class Application(tk.Tk):
             command=self.chbtnAutoClick,
         )
 
-
         self.btnDownload = tk.Button(
             self.tickFrame, text="Stáhnout kurzovní lístek", command=self.download
         )
         self.tickFrame.pack(anchor="w", padx=5)
         self.chboxAuto.pack()
         self.btnDownload.pack()
-        
+
         self.lblFrame = tk.LabelFrame(self, text="Transakce")
         self.lblFrame.pack(anchor="w", padx=5)
         self.varTransaction = tk.StringVar()
         self.varTransaction.set("None")
         self.rbtnPurchase = tk.Radiobutton(
-            self.lblFrame, value="purchace", variable=self.varTransaction, text="Nákup", command=self.changeTransaction
+            self.lblFrame,
+            value="purchace",
+            variable=self.varTransaction,
+            text="Nákup",
+            command=self.changeTransaction,
         )
         self.rbtnPurchase.pack()
         self.rbtnSale = tk.Radiobutton(
-            self.lblFrame, text="Prodej", variable=self.varTransaction, value="sale", command=self.changeTransaction
+            self.lblFrame,
+            text="Prodej",
+            variable=self.varTransaction,
+            value="sale",
+            command=self.changeTransaction,
         )
         self.rbtnSale.pack()
-        
+
         self.currFrame = tk.LabelFrame(self, text="Mněna")
         self.currFrame.pack(anchor="w", padx=5)
         self.combo = ttk.Combobox(self.currFrame, textvariable=self.currency)
@@ -104,37 +99,47 @@ class Application(tk.Tk):
         self.combo.current(0)
         self.combo.pack()
         self.combo.bind("<<ComboboxSelected>>", self.on_select)
-       
+
         self.rateFrame = tk.LabelFrame(self, text="Kurz")
         self.amLabel = tk.Label(self.rateFrame, text="UNKNOW")
         self.raLabel = tk.Label(self.rateFrame, text="CZK")
-        self.entryAmmount = MyEntry(self.rateFrame,textvariable=self.ammount ,state="readonly")
-        self.entryRate = MyEntry(self.rateFrame,textvariable=self.rate , state="readonly")
+        self.entryAmmount = MyEntry(
+            self.rateFrame, textvariable=self.ammount, state="readonly"
+        )
+        self.entryRate = MyEntry(
+            self.rateFrame, textvariable=self.rate, state="readonly"
+        )
         self.rateFrame.pack(anchor="w", padx=5)
         self.entryAmmount.grid(row=0)
-        self.amLabel.grid(row=0,column=1)
+        self.amLabel.grid(row=0, column=1)
         self.entryRate.grid(row=1)
         self.raLabel.grid(row=1, column=1)
 
         self.calcFrame = tk.LabelFrame(self, text="Výpočet")
-        self.calcInput = MyEntry(self.calcFrame, textvariable=self.input)
-        self.calcBtn = tk.Button(self.calcFrame, text="Výpočet", state="disable", command=self.calculate)
-        self.calcOutput = MyEntry(self.calcFrame, textvariable=self.output, state = "readonly")
+        self.calcInput = MyEntry(
+            self.calcFrame,
+            textvariable=self.input,
+            validate="all",
+            validatecommand=val_f_tuple,
+        )
+        self.calcBtn = tk.Button(
+            self.calcFrame, text="Výpočet", state="disable", command=self.calculate
+        )
+        self.calcOutput = MyEntry(
+            self.calcFrame, textvariable=self.output, state="readonly"
+        )
         self.inLabel = tk.Label(self.calcFrame, text="UNKNOW")
         self.ouLabel = tk.Label(self.calcFrame, text="CZK")
         self.calcFrame.pack(anchor="w", padx=5)
-        self.calcInput.grid(row=0,column=0)
+        self.calcInput.grid(row=0, column=0)
         self.inLabel.grid(row=0, column=1)
         self.calcBtn.grid(row=2, column=0)
-        self.calcOutput.grid(row=1,column=0)
+        self.calcOutput.grid(row=1, column=0)
         self.ouLabel.grid(row=1, column=1)
 
         self.btn = tk.Button(self, text="Quit", command=self.quit)
         self.btn.pack()
-        self.btn2 = tk.Button(self, text="About", command=self.about)
-        self.btn2.pack()
-        
-        self.ammount.set('')
+        self.ammount.set("")
 
         if os.path.exists("settings.txt"):
             with open("settings.txt", "r") as f:
@@ -158,14 +163,26 @@ class Application(tk.Tk):
 
     def load_date(self, file_date):
         date = dt.datetime.now()
-        self.tickState.configure(text=f"{file_date[:2]} {file_date[3:6]} {file_date[7:12]}")
-        if int(file_date[:2]) < date.day -1 or file_date[3:6] != date.strftime("%b") or int(file_date[7:12]) != date.year:      #          
+        self.tickState.configure(
+            text=f"{file_date[:2]} {file_date[3:6]} {file_date[7:12]}"
+        )
+        if (
+            int(file_date[:2]) < date.day - 1
+            or file_date[3:6] != date.strftime("%b")
+            or int(file_date[7:12]) != date.year
+        ):  #
             self.tickState.config(bg="red")
-            messagebox.showwarning(title="Lístek není aktuální",message="Váš kurzovní lístek není aktuální,\n pokračujte na vlastní nebezpečí!")
+            messagebox.showwarning(
+                title="Lístek není aktuální",
+                message="Váš kurzovní lístek není aktuální,\n pokračujte na vlastní nebezpečí!",
+            )
         else:
             self.tickState.config(bg="green")
 
-    def on_select(self,event = None):
+    def validate(self, P: str):
+        return bool(re.match(r"^[0-9]*([\.,][0-9]*)?$", P))
+
+    def on_select(self, event=None):
         self.output.set(0)
         selected = self.currency.get()
         if self.exrate == {}:
@@ -179,25 +196,37 @@ class Application(tk.Tk):
                 self.inLabel.config(text=info[2])
             except KeyError:
                 pass
-            if (self.varTransaction.get() == "purchace" or self.varTransaction.get() == "sale") and self.ammount.get() != '':
-                self.calcBtn.config(state= "normal")
-    
-    def changeTransaction(self,event = None):
+            if (
+                self.varTransaction.get() == "purchace"
+                or self.varTransaction.get() == "sale"
+            ) and self.ammount.get() != "":
+                self.calcBtn.config(state="normal")
+
+    def changeTransaction(self, event=None):
         self.output.set(0)
         if self.varTransaction.get() == "purchace":
             self.feemod = 1 - (0.01 * self.fee)
         else:
             self.feemod = 1 + (0.01 * self.fee)
         self.on_select(None)
-        if self.exrate != {} and self.ammount.get() != '':
-            self.calcBtn.config(state= "normal")
+        if self.exrate != {} and self.ammount.get() != "":
+            self.calcBtn.config(state="normal")
 
     def calculate(self, event=None):
         if self.varTransaction.get() == "purchace":
-            self.output.set(math.floor((self.input.get()/float(self.ammount.get()))*float(self.rate.get())))
+            self.output.set(
+                math.floor(
+                    (self.input.get() / float(self.ammount.get()))
+                    * float(self.rate.get())
+                )
+            )
         elif self.varTransaction.get() == "sale":
-            self.output.set(math.ceil((self.input.get()/float(self.ammount.get()))*float(self.rate.get())))
-
+            self.output.set(
+                math.ceil(
+                    (self.input.get() / float(self.ammount.get()))
+                    * float(self.rate.get())
+                )
+            )
 
     def chbtnAutoClick(self, event=None):
         with open("settings.txt", "w") as f:
@@ -210,21 +239,16 @@ class Application(tk.Tk):
                 f.write("0")
                 f.close()
                 self.btnDownload.configure(state="normal")
-            
-        
-
-    def about(self):
-        window = About(self)
-        window.grab_set()
-
-    def quit(self, event=None):
-        super().quit()
 
     def load_ticket(self, ticket):
         for line in ticket.splitlines()[2:]:
             country, currency, amount, code, rate = line.split("|")
             self.exrate[f"{country}({code})"] = [float(amount), float(rate), str(code)]
         self.combo.config(values=list(self.exrate.keys()))
+
+        if self.currency.get() == "Nenalezeno!":
+            self.combo.current(0)
+            self.on_select()
 
     def download(self):
         URL = "https://www.cnb.cz/en/financial_markets/foreign_exchange_market/exchange_rate_fixing/daily.txt"
@@ -235,11 +259,14 @@ class Application(tk.Tk):
                 f.write(data)
         except requests.exceptions.ConnectionError as e:
             print(f"Error: {e}")
-            messagebox.showerror(message=f"Nebylo možné stáhonut kurzovní lístke.\nZkontrolujet přípojení k internetu.\n Kód chyby: {e}", title="Chyba stahování")
+            messagebox.showerror(
+                message=f"Nebylo možné stáhonut kurzovní lístke.\nZkontrolujet přípojení k internetu.\n Kód chyby: {e}",
+                title="Chyba stahování",
+            )
 
         if os.path.exists("kurzovni_listek.txt"):
             with open("kurzovni_listek.txt", "r") as f:
-                date_info = f.readline()                    
+                date_info = f.readline()
                 data = f.read()
                 f.close()
         else:
@@ -247,8 +274,6 @@ class Application(tk.Tk):
             self.destroy()
         self.load_ticket(data)
         self.load_date(date_info)
-
-        
 
 
 app = Application()
